980742b2bd347f27e0e120319be59636c0354446
brinnatt <brinnatt@163.com>
2025-07-26 21:18:01 +0800

    [rollback] pyinstaller does not work

--- kubecli.py
+++ kubecli.py
@@ -1,19 +1,8 @@
-import os
-from pathlib import Path
-
 from core.cli import KubeautoCLI
-
-
-def inject_ansible_cfg():
-    cfg_path = Path(__file__).parent / "ansible.cfg"
-    if not cfg_path.exists():
-        raise FileNotFoundError("ansible.cfg not found")
-    os.environ["ANSIBLE_CONFIG"] = str(cfg_path)
 
 
 def main():
     """Entry point for the CLI"""
-    inject_ansible_cfg()
     cli = KubeautoCLI()
     cli.run()
 
