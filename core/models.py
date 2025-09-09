800cfc38096b698b85860ce1c0272a411d5e59d3
brinnatt <brinnatt@163.com>
2025-09-09 22:04:40 +0800

    [remove] useless model

--- core/models.py
+++ core/models.py
@@ -1,26 +1,3 @@
 """
-Data models for kubeauto
+Data models will be used in the future
 """
-from dataclasses import dataclass
-from typing import List, Optional, Dict, Any
-
-@dataclass
-class Cluster:
-    name: str
-    config: Dict[str, Any]
-    hosts: List[str]
-    kubeconfig: Optional[str] = None
-
-@dataclass
-class Node:
-    ip: str
-    role: str  # 'etcd', 'master', 'node'
-    cluster: str
-    extra_info: Optional[Dict[str, str]] = None
-
-@dataclass
-class KubeConfigUser:
-    name: str
-    user_type: str  # 'admin' or 'view'
-    expiry: str
-    cert_path: str
